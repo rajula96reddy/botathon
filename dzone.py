a6a183df32a8990a03b7377acd18e4c847b7e0cd
rajula96reddy <rajula96reddy@gmail.com>
2017-10-31 04:34:26 +0530

    added api for movies with basic working

--- dzone.py
+++ dzone.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/currency', methods=['GET'])
 
-def getAllEmp():
+def getCurrencies():
     cc = request.args.get('cryptocurrency')
     c = request.args.get('currency')
     url = "https://api.coinmarketcap.com/v1/ticker/" + cc + "/?convert=" +c
@@ -36,6 +36,26 @@ def getAllEmp():
     print output_str
     return json.dumps(engati_format, sort_keys=True, indent=4)
 
+@app.route('/movie', methods=['GET'])
+
+def getMovies():
+    a = request.args.get('type')
+    key = "11a50d9e0d4e6e2f636e6af1cf440919"
+    url = "https://api.themoviedb.org/3/movie/popular"+"?language=en-US&api_key="+key
+    # payload = "{}"
+    response = urllib2.urlopen(url)
+    # url="https://api.coinsecure.in/v1/exchange/ticker"
+    # response = urllib2.urlopen(url)
+    data = json.loads(response.read())
+    textdata=""
+    for i in range(len(data["results"])):
+        textdata = textdata +", "+ str(data["results"][i]["title"])
+    engati_format = {
+"data": {
+"type": "text",
+"text": "The list of popular movies are: " + textdata}
+}
+    return json.dumps(engati_format)
 
 if __name__ == '__main__':
     app.run()
